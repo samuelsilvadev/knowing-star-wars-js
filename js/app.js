35f3d94962a4a106f711a4e29d047f1166cdd791
samuel <samuelsilvawb@gmail.com>
2017-03-01 21:41:44 -0300

    adicionando alguns tratamentos

--- js/app.js
+++ js/app.js
@@ -4,9 +4,23 @@ function fazRequestGet(url, callback){
 	var req = new XMLHttpRequest();
 	req.open("GET", url, true);
 	req.addEventListener("load",function(){
-		console.log(req.responseText);	
+		if (req.status < 400)
+			callback(req.responseText);
+		else
+			callback(null, new Error("Requisição falhou: " + req.statusText));
+	});
+	req.addEventListener("error", function() {
+		callback(null, new Error("Erro na Rede"));
 	});
 	req.send(null);
 }
 
-fazRequestGet("http://swapi.co/api/people/");
+
+
+	fazRequestGet("http://swapi.co/api/peopl/", function(conteudo, error){
+		if(error != null)
+			console.log("ERRO " + error)
+		else
+			console.log(conteudo);
+	});
+
