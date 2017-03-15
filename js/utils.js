71b2581d7588f2c7312678b31a115e787b444e53
samuel <samuelsilvawb@gmail.com>
2017-03-15 20:09:21 -0300

    criando utilitario para usar funções repetidas

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+
+function addElement(element){
+	var newElement = document.createElement(element);
+	return newElement;
+}
+
+function addTextInElement(element, text){
+	var newContent = document.createTextNode(text);
+	element.appendChild(newContent);
+	return element;
+}
