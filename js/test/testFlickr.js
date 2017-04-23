41fe8d80fa2430d09de30d4b76540503e7acf122
samuel <samuelsilvawb@gmail.com>
2017-04-23 12:31:26 -0300

    estilizando | usando lib do flickr

--- js/test/testFlickr.js
+++ js/test/testFlickr.js
@@ -1,24 +1,21 @@
 
-var Flickr = require(["flickrapi"]),
-	flickrOptions = {
+let flickr = new Flickr({
 	api_key: "85cbbaacf76f1a89b73310aac077de09",
 	secret: "efb97e4ac51ac134",
 	requestOptions: {
-	timeout: 20000,
+	timeout: 20000
 	/* other default options accepted by request.defaults */
-	}
-};
-
-let flickr = new Flickr();
+}});
 
 let result = flickr.photos.search({
-  text: "red+panda"
-}, function(err, result) {
+  text: "Luke skywalker"
+}, function(err, resultado) {
   if(err) { throw new Error(err); }
+  console.log(resultado);
   // do something with result
 });
 
-result.addEventListener("load", () => {
+/*result.addEventListener("load", () => {
 	console.log(result);
-})
+})*/
 
